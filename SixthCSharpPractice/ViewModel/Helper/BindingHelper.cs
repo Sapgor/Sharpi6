0ee31797e4b11f4d04e344675ffcfcad7f2d1097
Sapgor <vasyuckov.n@yandex.ru>
2024-09-23 00:30:42 +0300

    календарь еды

---
+++ SixthCSharpPractice/ViewModel/Helper/BindingHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SixthCSharpPractice.ViewModel.Helper;
+
+public class BindingHelper : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
